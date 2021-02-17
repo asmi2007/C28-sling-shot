90de92ebdf6140c780363d2de0d4cba95e0a4600
asmi2007 <76519101+asmi2007@users.noreply.github.com>
2021-02-17 17:40:01 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,35 @@
+class Chain{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB;
+
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+
+    fly()
+    {
+
+        this.chain.bodyA = null; // null means empty 
+    }
+
+
+    display(){
+
+if (this.chain.bodyA)
+
+{
+    var pointA = this.chain.bodyA.position;
+    var pointB = this.pointB;
+    strokeWeight(4);
+    line(pointA.x, pointA.y, pointB.x, pointB.y);
+}
+      
+    }
+    
+}
